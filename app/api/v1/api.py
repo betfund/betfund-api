6d9f7dab7a5e6b8251069c4621cc55188950d00a
Leon Kozlowski <LeonKozlowski@gmail.com>
2020-05-07 21:53:19 -0400

    Feature/nosql (#2)
    * nosql Motor mongo client support
    
    * added sport_id parameter

--- app/api/v1/api.py
+++ app/api/v1/api.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.v1.endpoints import funds, login, users, user_ledgers, utils
+from app.api.v1.endpoints import events, funds, login, users, user_ledgers, utils
 
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["Login"])
@@ -8,3 +8,4 @@ api_router.include_router(users.router, prefix="/users", tags=["Users"])
 api_router.include_router(funds.router, prefix="/funds", tags=["Funds"])
 api_router.include_router(user_ledgers.router, prefix="/user-ledgers", tags=["User Ledgers"])
 api_router.include_router(utils.router, prefix="/utils", tags=["Utils"])
+api_router.include_router(events.router, prefix="/events", tags=["events"])
---
+++ app/db/nosql/__init__.py
@@ -0,0 +1,1 @@
+"""NoSql DB Modules."""
